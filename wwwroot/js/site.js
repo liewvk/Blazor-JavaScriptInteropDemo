c0ce9dc2a46c436e83e5a8d5cdadb016c3c73495
Dr. Liew vk <liewvk@yahoo.com>
2026-09-16 12:37:50 +0800

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿window.getWindowTitle = function () {
+    return document.title;
+};
+
+window.setWindowTitle = function (title) {
+    document.title = title;
+};
+
+window.scrollToBottom = function () {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+};
